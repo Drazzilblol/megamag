b25ba1cb2ac37b735b2c77fc32ff85b5305a3c3d
Andrei Pretskailo <andrei.pretskaila@instinc.tools>
2017-06-09 13:28:33 +0300

    use glide blur instead custom, fix recycle unnecessary bm

--- app/src/main/java/by/instinctools/megamag/common/utils/BlurAndCropTransformation.java
+++ app/src/main/java/by/instinctools/megamag/common/utils/BlurAndCropTransformation.java
@@ -6,33 +6,55 @@ import android.graphics.Canvas;
 import android.support.annotation.NonNull;
 
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapResource;
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
 
-import timber.log.Timber;
+import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class BlurAndCropTransformation extends BitmapTransformation {
 
+    private BlurTransformation blur;
+
     public BlurAndCropTransformation(Context context) {
         super(context);
+        blur = new BlurTransformation(context);
     }
 
     @Override
     protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
-        if (toTransform.getWidth() > outWidth && toTransform.getHeight() > outHeight) {
-            return addBackgroundBlur(resizeBitmap(toTransform, outWidth, outHeight));
-        } else {
-            return addBackgroundBlur(toTransform);
+        Bitmap srcBitmap = toTransform;
+        if (srcBitmap.getWidth() > outWidth && srcBitmap.getHeight() > outHeight) {
+            srcBitmap = resizeBitmap(srcBitmap, outWidth, outHeight);
+        }
+        try {
+            return addBackgroundBlur(srcBitmap, pool, outWidth, outHeight);
+        } finally {
+            if (srcBitmap != toTransform) {
+                srcBitmap.recycle();
+            }
         }
     }
 
     private Bitmap resizeBitmap(Bitmap toTransform, int outWidth, int outHeight) {
-        int bmWidth = toTransform.getWidth();
-        int bmHeight = toTransform.getHeight();
-        while (bmWidth > outWidth && bmHeight > outHeight) {
-            bmWidth /= 1.5;
-            bmHeight /= 1.5;
+
+        float bmWidth = toTransform.getWidth();
+        float bmHeight = toTransform.getHeight();
+
+        float widthDiff = bmWidth / outWidth;
+        float heightDiff = bmHeight / outHeight;
+
+        int resWidth;
+        int resHeight;
+
+        if (widthDiff > heightDiff) {
+            resWidth = (int) (bmWidth / heightDiff);
+            resHeight = (int) (bmHeight / heightDiff);
+        } else {
+            resWidth = (int) (bmWidth / widthDiff);
+            resHeight = (int) (bmHeight / widthDiff);
         }
-        return Bitmap.createScaledBitmap(toTransform, bmWidth, bmHeight, true);
+
+        return Bitmap.createScaledBitmap(toTransform, resWidth, resHeight, true);
     }
 
     @Override
@@ -41,23 +63,29 @@ public class BlurAndCropTransformation extends BitmapTransformation {
     }
 
     @NonNull
-    private static Bitmap addBackgroundBlur(@NonNull Bitmap bitmap) {
-        Bitmap cropped = cropDrawable(bitmap);
-        Bitmap blurred = blur(cropped, 7);
+    private Bitmap addBackgroundBlur(@NonNull Bitmap bitmap, BitmapPool pool, int outWidth, int outHeight) {
+        Bitmap cropped = cropDrawable(bitmap, outWidth, outHeight);
+
+        Bitmap blurred = blur.transform(BitmapResource.obtain(cropped, pool),
+                cropped.getWidth(),
+                cropped.getHeight())
+                .get();
         cropped.recycle();
 
         float width = blurred.getHeight() * ((float) bitmap.getWidth() / bitmap.getHeight());
-        Bitmap src = Bitmap.createScaledBitmap(
+        Bitmap scaledSrcBitmap = Bitmap.createScaledBitmap(
                 bitmap,
                 (int) width,
                 blurred.getHeight(),
                 true);
         Canvas canvas = new Canvas(blurred);
-        canvas.drawBitmap(src, blurred.getWidth() / 2 - src.getWidth() / 2, 0f, null);
+        canvas.drawBitmap(scaledSrcBitmap, blurred.getWidth() / 2 - scaledSrcBitmap.getWidth() / 2, 0f, null);
+        scaledSrcBitmap.recycle();
         return blurred;
     }
 
-    private static Bitmap cropDrawable(@NonNull Bitmap source) {
+    private static Bitmap cropDrawable(@NonNull Bitmap source, int outWidth, int outHeight) {
+        float aspectRatio = (float) outWidth / outHeight;
         Bitmap bitmap;
         if (source.getWidth() >= source.getHeight()) {
             bitmap = Bitmap.createBitmap(
@@ -65,7 +93,7 @@ public class BlurAndCropTransformation extends BitmapTransformation {
                     source.getWidth() / 2 - source.getHeight() / 2,
                     0,
                     source.getHeight(),
-                    (int) (source.getHeight() / 1.76)
+                    (int) (source.getHeight() / aspectRatio)
             );
         } else {
             bitmap = Bitmap.createBitmap(
@@ -73,212 +101,9 @@ public class BlurAndCropTransformation extends BitmapTransformation {
                     0,
                     source.getHeight() / 2 - source.getWidth() / 2,
                     source.getWidth(),
-                    (int) (source.getWidth() / 1.76)
+                    (int) (source.getWidth() / aspectRatio)
             );
         }
         return bitmap;
-    }
-
-    @NonNull
-    private static Bitmap blur(@NonNull Bitmap sentBitmap, int radius) {
-        Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
-
-        if (radius < 1) {
-            radius = 1;
-        }
-
-        int w = bitmap.getWidth();
-        int h = bitmap.getHeight();
-
-        int[] pix = new int[w * h];
-        Timber.e(w + " " + h + " " + pix.length);
-        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
-
-        int wm = w - 1;
-        int hm = h - 1;
-        int wh = w * h;
-        int div = radius + radius + 1;
-
-        int r[] = new int[wh];
-        int g[] = new int[wh];
-        int b[] = new int[wh];
-        int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
-        int vmin[] = new int[Math.max(w, h)];
-
-        int divsum = (div + 1) >> 1;
-        divsum *= divsum;
-        int dv[] = new int[256 * divsum];
-        for (i = 0; i < 256 * divsum; i++) {
-            dv[i] = (i / divsum);
-        }
-
-        yw = yi = 0;
-
-        int[][] stack = new int[div][3];
-        int stackpointer;
-        int stackstart;
-        int[] sir;
-        int rbs;
-        int r1 = radius + 1;
-        int routsum, goutsum, boutsum;
-        int rinsum, ginsum, binsum;
-
-        for (y = 0; y < h; y++) {
-            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
-            for (i = -radius; i <= radius; i++) {
-                p = pix[yi + Math.min(wm, Math.max(i, 0))];
-                sir = stack[i + radius];
-                sir[0] = (p & 0xff0000) >> 16;
-                sir[1] = (p & 0x00ff00) >> 8;
-                sir[2] = (p & 0x0000ff);
-                rbs = r1 - Math.abs(i);
-                rsum += sir[0] * rbs;
-                gsum += sir[1] * rbs;
-                bsum += sir[2] * rbs;
-                if (i > 0) {
-                    rinsum += sir[0];
-                    ginsum += sir[1];
-                    binsum += sir[2];
-                } else {
-                    routsum += sir[0];
-                    goutsum += sir[1];
-                    boutsum += sir[2];
-                }
-            }
-            stackpointer = radius;
-
-            for (x = 0; x < w; x++) {
-
-                r[yi] = dv[rsum];
-                g[yi] = dv[gsum];
-                b[yi] = dv[bsum];
-
-                rsum -= routsum;
-                gsum -= goutsum;
-                bsum -= boutsum;
-
-                stackstart = stackpointer - radius + div;
-                sir = stack[stackstart % div];
-
-                routsum -= sir[0];
-                goutsum -= sir[1];
-                boutsum -= sir[2];
-
-                if (y == 0) {
-                    vmin[x] = Math.min(x + radius + 1, wm);
-                }
-                p = pix[yw + vmin[x]];
-
-                sir[0] = (p & 0xff0000) >> 16;
-                sir[1] = (p & 0x00ff00) >> 8;
-                sir[2] = (p & 0x0000ff);
-
-                rinsum += sir[0];
-                ginsum += sir[1];
-                binsum += sir[2];
-
-                rsum += rinsum;
-                gsum += ginsum;
-                bsum += binsum;
-
-                stackpointer = (stackpointer + 1) % div;
-                sir = stack[(stackpointer) % div];
-
-                routsum += sir[0];
-                goutsum += sir[1];
-                boutsum += sir[2];
-
-                rinsum -= sir[0];
-                ginsum -= sir[1];
-                binsum -= sir[2];
-
-                yi++;
-            }
-            yw += w;
-        }
-        for (x = 0; x < w; x++) {
-            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
-            yp = -radius * w;
-            for (i = -radius; i <= radius; i++) {
-                yi = Math.max(0, yp) + x;
-
-                sir = stack[i + radius];
-
-                sir[0] = r[yi];
-                sir[1] = g[yi];
-                sir[2] = b[yi];
-
-                rbs = r1 - Math.abs(i);
-
-                rsum += r[yi] * rbs;
-                gsum += g[yi] * rbs;
-                bsum += b[yi] * rbs;
-
-                if (i > 0) {
-                    rinsum += sir[0];
-                    ginsum += sir[1];
-                    binsum += sir[2];
-                } else {
-                    routsum += sir[0];
-                    goutsum += sir[1];
-                    boutsum += sir[2];
-                }
-
-                if (i < hm) {
-                    yp += w;
-                }
-            }
-            yi = x;
-            stackpointer = radius;
-            for (y = 0; y < h; y++) {
-                // Preserve alpha channel: ( 0xff000000 & pix[yi] )
-                pix[yi] = (0xff000000 & pix[yi]) | (dv[rsum] << 16) | (dv[gsum] << 8) | dv[bsum];
-
-                rsum -= routsum;
-                gsum -= goutsum;
-                bsum -= boutsum;
-
-                stackstart = stackpointer - radius + div;
-                sir = stack[stackstart % div];
-
-                routsum -= sir[0];
-                goutsum -= sir[1];
-                boutsum -= sir[2];
-
-                if (x == 0) {
-                    vmin[y] = Math.min(y + r1, hm) * w;
-                }
-                p = x + vmin[y];
-
-                sir[0] = r[p];
-                sir[1] = g[p];
-                sir[2] = b[p];
-
-                rinsum += sir[0];
-                ginsum += sir[1];
-                binsum += sir[2];
-
-                rsum += rinsum;
-                gsum += ginsum;
-                bsum += binsum;
-
-                stackpointer = (stackpointer + 1) % div;
-                sir = stack[stackpointer];
-
-                routsum += sir[0];
-                goutsum += sir[1];
-                boutsum += sir[2];
-
-                rinsum -= sir[0];
-                ginsum -= sir[1];
-                binsum -= sir[2];
-
-                yi += w;
-            }
-        }
-
-        bitmap.setPixels(pix, 0, w, 0, 0, w, h);
-
-        return (bitmap);
     }
 }
